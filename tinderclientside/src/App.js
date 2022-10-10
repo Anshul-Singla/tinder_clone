c5e6f63d479ed0fbcbad598b5bedddee6e2c6ae5
Anshul-Singla <asingla2807@gmail.com>
2022-10-11 02:53:23 +0530

    setup for frontend

---
+++ tinderclientside/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build the MERN TINDER clone (LIFE IS AWESOME...) </h1>
+    {/* Header */}
+    {/* TinderCard */}
+    {/* Swipe Button */}
+    </div>
+  );
+}
+
+export default App;
